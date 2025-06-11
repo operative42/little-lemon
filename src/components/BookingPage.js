21b9d76b7d46138ed47e73c08aa5cf960fa5623c
Operative42 <operative42@gmail.com>
2025-06-11 15:12:54 -0700

    Adding booking and testimonials

---
+++ src/components/BookingPage.js
@@ -0,0 +1,32 @@
+import React,{ useReducer, useState } from 'react'
+import BookingForm from './BookingForm'
+import { useNavigate } from 'react-router-dom';
+
+
+import { initializeTimes, updateTimes } from './TimeReducer';
+
+
+
+export default function BookingPage() {
+    const navigate = useNavigate();
+
+    const [AvailableTimes, dispatch]=useReducer(updateTimes, [], initializeTimes )
+    const [AvailableTime, setAvalaibleTime]=useState("")
+
+
+    return (
+        <>
+            <button 
+            className="back-button"
+            onClick={() => navigate('/')}
+            >
+            Retour à l&apos;accueil
+            </button>
+
+            <BookingForm AvailableTime={AvailableTime} setAvalaibleTime={setAvalaibleTime} dispatch={dispatch} AvailableTimes={AvailableTimes} />
+        </>
+    )
+}
+
+
+
